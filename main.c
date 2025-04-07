57326fb644808e89c4e5c9df6e08eba4b3b02850
AnkitaSuryvnshi <ankitasuryvnshi@gmail.com>
2025-04-07 11:03:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,39 @@
-#include <stdio.h>
 
-int custom_muladd(int A, int B, int C, int D) {
-    return (A + B) * (C - D);
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+// Function to simulate generating assembly code
+void generateAssembly(string reg1, string reg2, string op) {
+    if (op == "+") {
+        cout << "ADD " << reg1 << ", " << reg2 << endl;
+    } else if (op == "-") {
+        cout << "SUB " << reg1 << ", " << reg2 << endl;
+    } else {
+        cerr << "Unsupported operation: " << op << endl;
+    }
 }
 
-int main() {
-    int A = 5, B = 3, C = 10, D = 4;
-    int Z = custom_muladd(A, B, C, D);
-    printf("A = %d, B = %d, C = %d, D = %d\\n", A, B, C, D);
-    printf("Z = (A + B) * (C - D) = %d\\n", Z);
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+        return 1;
+    }
+
+    ifstream infile(argv[1]);
+    if (!infile) {
+        cerr << "Error opening file: " << argv[1] << endl;
+        return 1;
+    }
+
+    string var1, var2, var3, op1, op2;
+    infile >> var1 >> op1 >> var2 >> op2 >> var3;
+
+    cout << "; Assembly Code Generated" << endl;
+    generateAssembly(var1, var2, op1);
+    generateAssembly(var1, var3, op2);
+
     return 0;
 }
