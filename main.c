318042420ce68a423f411eab7962da3a0b16a9ef
AnkitaSuryvnshi <ankitasuryvnshi@gmail.com>
2025-04-07 11:53:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,16 @@
+#include <stdio.h>
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-using namespace std;
-
-// Function to simulate generating assembly code
-void generateAssembly(string reg1, string reg2, string op) {
-    if (op == "+") {
-        cout << "ADD " << reg1 << ", " << reg2 << endl;
-    } else if (op == "-") {
-        cout << "SUB " << reg1 << ", " << reg2 << endl;
-    } else {
-        cerr << "Unsupported operation: " << op << endl;
-    }
+// Simulate the custom instruction: Z = A + B - C
+int custom_addsub(int A, int B, int C) {
+    return A + B - C;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
-        return 1;
-    }
+int main() {
+    int A = 10, B = 5, C = 3;
+    int Z = custom_addsub(A, B, C);
 
-    ifstream infile(argv[1]);
-    if (!infile) {
-        cerr << "Error opening file: " << argv[1] << endl;
-        return 1;
-    }
-
-    string var1, var2, var3, op1, op2;
-    infile >> var1 >> op1 >> var2 >> op2 >> var3;
-
-    cout << "; Assembly Code Generated" << endl;
-    generateAssembly(var1, var2, op1);
-    generateAssembly(var1, var3, op2);
+    printf("A = %d, B = %d, C = %d\n", A, B, C);
+    printf("Z = A + B - C = %d\n", Z);
 
     return 0;
 }
